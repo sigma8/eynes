33acf60978ad4360a0eae452804bc907058d4b2f
jtortolero <josegabrieltortolero@gmail.com>
2022-02-21 10:48:16 -0300

    clases v3 print

--- clases.py
+++ clases.py
@@ -1,13 +1,16 @@
 from math import pi
+from builtins import Exception
 
-from pip import main
+
 
 class Circulo:
 
-
     def __init__(self, radius): 
         self.radius = radius
-
+    def __str__(self) -> str:
+        return f"Radio del circulo {self.radius}"
+    def __repr__(self) -> str:
+        return f"Circulo (radio={self.radius}"
     @property
     def radius(self):
         return self._radius
@@ -17,18 +20,22 @@ class Circulo:
         if radius <= 0 :
             raise ValueError("El radio no puede ser menor o igual a cero")
         self._radius = radius
-        #if not isinstance(radius, (float, int)):
-        #    raise TypeError("Radio debe ser un valor mayor a cero")
-        #self._radius = float(radius)
-
+        if not isinstance(radius, (float, int)):
+            raise TypeError("Radio debe ser un valor mayor a cero")
+        self._radius = float(radius)
+    
     def get_area(self):
-        return self.radius ** 2 * pi
+        return f"El area del circulo es: {self.radius ** 2 * pi}"
+    
     
     def get_perimetro(self):
-        return self.radius * 2 * pi
+        return f"El perimetro del circulo es: {self.radius * 2 * pi}"
+    
 
 if __name__ == '__main__':
+    
+    new_circulo = Circulo(4)
 
-    new_circulo = Circulo(2)
+    print(new_circulo, new_circulo.get_area(), new_circulo.get_perimetro())
 
-    print(new_circulo.get_area())
+
